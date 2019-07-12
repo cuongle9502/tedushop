6e2002e4394e21b369c43466708550007d9b0fab
lecuong9502 <huathithuong.qn315@gmail.com>
2019-07-12 14:09:28 +0700

    bai 6 xay dung data Access layer

---
+++ TeduShop.Date/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Date.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        TedushopDbContext dbContext;
+
+        public TedushopDbContext Iint()
+        {
+            return dbContext ?? (dbContext = new TedushopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ TeduShop.Date/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Date.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private TedushopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public TedushopDbContext DbContext
+        {
+            get { return DbContext ?? (dbContext = dbFactory.Iint()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
